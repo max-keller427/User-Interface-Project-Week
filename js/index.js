7cdc4090c632f0b2a8774de2cc4e99ec352148a2
max-keller427 <max.keller427@gmail.com>
2019-02-19 21:03:54 -0700

    dropdown menu working!

--- js/index.js
+++ js/index.js
@@ -3,6 +3,17 @@
 class Dropdown {
   constructor(element) {
     this.element = element;
-    this.anchor = element.querySelector("dropdown-button");
+    this.button = element.querySelector(".dropdown-button");
+    this.content = element.querySelector(".nav-links");
+    this.button.addEventListener("click", () => {
+      this.toggleContent();
+    });
+  }
+  toggleContent() {
+    this.content.classList.toggle("nav-links-hidden");
   }
 }
+
+let dropDowns = document
+  .querySelectorAll(".dropdown")
+  .forEach(link => new Dropdown(link));
